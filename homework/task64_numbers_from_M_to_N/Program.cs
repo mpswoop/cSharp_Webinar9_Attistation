86eb196fddc53d358f6a66bfb28d4ba4e37adace
MP <mp@swoop.pro>
2023-08-11 00:50:53 +0300

    task 64 done - write numbers from M to N

---
+++ homework/task64_numbers_from_M_to_N/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("input number M");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number N");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+string Numbers(int numM, int numN)
+
+{
+    if (numM <= numN)
+    
+        return $"{numM} " + Numbers(numM+1,numN);
+    
+    else return string.Empty;
+}
+Console.WriteLine(Numbers(numM, numN));
+
+
