59e3f9ce9f76f70808c238b440fbd9b3f0ebb7f0
MP <mp@swoop.pro>
2023-08-11 17:09:28 +0300

    task68 done - Ackermann Fuction

---
+++ homework/task68_Ackerman_Fuction/Program.cs
@@ -0,0 +1,19 @@
+﻿// ackermann function
+Console.WriteLine("input number M");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number N");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int Ackerman(int numM, int numN)
+{
+    if (numM == 0)
+        return numN + 1;
+    else if (numN == 0)
+        return Ackerman(numM - 1, 1);
+    else
+        return Ackerman(numM - 1, Ackerman(numM, numN - 1));
+
+}
+
+int result = Ackerman(numM, numN);
+Console.WriteLine(result);
