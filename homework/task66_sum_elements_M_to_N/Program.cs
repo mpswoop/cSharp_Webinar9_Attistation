38b9862c4ba55dc6b1a198f49e6c83f4adfd670f
MP <mp@swoop.pro>
2023-08-11 12:15:31 +0300

    task 66 done - calculate sum of elements from M to N

---
+++ homework/task66_sum_elements_M_to_N/Program.cs
@@ -0,0 +1,16 @@
+﻿// Sum of eleents from M to N
+Console.WriteLine("input number M");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number N");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int SumElements(int numM, int numN)
+{
+    if (numM <= numN)
+        return numM + SumElements(numM + 1, numN);
+    else return 0;
+
+}
+
+int result = SumElements(numM, numN);
+Console.WriteLine(result);
